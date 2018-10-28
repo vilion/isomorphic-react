6e5b832540df4b8b4e3448690b9611dbe7c9b9a1
virtualion <takabatakekoichi@gmail.com>
2018-10-28 07:33:09 +0000

    add stateless component test.

---
+++ src/components/__tests__/QuestionDetail.js
@@ -0,0 +1,35 @@
+import { mapStateToProps, QuestionDetailDisplay } from '../QuestionDetail';
+import renderer from 'react-test-renderer';
+import React from 'react';
+
+describe('The Question Detail Component',()=>{
+    describe('The Container Element',()=>{
+        describe('mapStateToProps', ()=>{
+            it('Should map the state to props correctly', ()=>{
+                const appState = {
+                    questions:[{question_id:42}]
+                };
+                const ownProps = {
+                    question_id: 42
+                };
+                const componentState = mapStateToProps(appState, ownProps);
+                expect(componentState).toEqual({question_id:42});
+            });
+        });
+    });
+
+    describe('The display element', ()=>{
+        it('Should not regress', ()=>{
+            const tree = renderer.create(
+                <QuestionDetailDisplay
+                    titile="The meaning of life"
+                    body="42"
+                    answer_count={0}
+                    tags={['hitchhiking']}
+                />
+            );
+            expect(tree.toJSON()).toMatchSnapshot();
+        });
+    });
+
+});
